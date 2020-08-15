56810c5ba5f36db2f033dd7815d4a681f5bb4605
lushaoshu <51908698+lushaoshu@users.noreply.github.com>
2020-08-15 15:45:37 +0800

    Add files via upload

---
+++ Assets/Scripts/game/CommonPanel/StartPanel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPanel : MyPanel
+{
+
+    public StartPanel()
+    {
+        _panelResName = "Start_panel";
+    }
+
+    public override void OnInit()
+    {
+        base.OnInit();
+
+
+    }
+
+    public override void OnShow()
+    {
+        base.OnShow();
+
+    }
+
+    public void __OnPointerDown(object btn)
+    {
+
+    }
+
+    public void __OnPointerUp(object btn)
+    {
+
+    }
+
+    public void OnClickEvent(object btn)
+    {
+        GameObject GoBtn = (GameObject)btn;
+        if (GoBtn.name == "Start")
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+
+
+
+}
